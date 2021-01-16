e5991dd67096a057c5572ecd2484859cccfafe9d
laineus <lainen000@gmail.com>
2021-01-17 00:48:24 +0900

    promise

--- src/convert.js
+++ src/convert.js
@@ -1,18 +1,18 @@
 const path = require('path')
-const fs = require('fs')
-const convert = require('./autotile')
+const fs = require('fs').promises
+const autotile = require('./autotile')
 
-module.exports = (input, outputDir, size) => {
+module.exports = async (input, outputDir, size) => {
   const filename = path.basename(input)
-  convert(input, size).then(buffer => {
-    return fs.writeFileSync(path.join(outputDir, filename), buffer)
-  })
+  const buffer = await autotile(input, size)
+  await fs.writeFile(path.join(outputDir, filename), buffer)
   const keyName = filename.split('.')[0]
-  const ruleTmx = fs.readFileSync(path.resolve(__dirname, './rule.tmx'), 'utf-8')
-                    .replace(/\{FILE_NAME\}/g, filename)
-                    .replace(/\{TILE_SIZE\}/g, size)
-                    .replace(/\{LAYER_NAME\}/g, keyName)
-                    .replace(/\{WIDTH\}/g, size * 8)
-                    .replace(/\{HEIGHT\}/g, size * 6)
-  fs.writeFileSync(path.join(outputDir, `${keyName}.tmx`), ruleTmx)
+  const ruleTmx = await fs.readFile(path.resolve(__dirname, './rule.tmx'), 'utf-8').then(v => {
+    return v.replace(/\{FILE_NAME\}/g, filename)
+            .replace(/\{TILE_SIZE\}/g, size)
+            .replace(/\{LAYER_NAME\}/g, keyName)
+            .replace(/\{WIDTH\}/g, size * 8)
+            .replace(/\{HEIGHT\}/g, size * 6)
+  })
+  await fs.writeFile(path.join(outputDir, `${keyName}.tmx`), ruleTmx)
 }
